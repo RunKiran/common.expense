02ac54c22744619a868165997a915a5103442431
RunKiran <kiranmadasu790@gmail.com>
2024-04-27 17:37:45 +0530

    some-message

--- common.sh
+++ common.sh
@@ -3,7 +3,8 @@ set -e
 Handdle_error(){
     echo "error occured at $1: ,error command:$2"
 }
-trap 'Handdle_error at ${lineNO}'"BASH_COMMAND"'ERR
+
+trap ' Handdle_error at ${lineNO}'"BASH_COMMAND"' ERR
 USERID=$(id -u)
 TIME_STAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
